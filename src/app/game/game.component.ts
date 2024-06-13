f66a0861f51fa21de3975d85a774e2030e8cbdbb
NguyenH-T <89490954+NguyenH-T@users.noreply.github.com>
2024-06-13 17:09:54 -0600

    Tiles now calculate endpoint when moving left

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -18,7 +18,7 @@ class TileData {
 //The chance for a tile to have a value of 4 instead of 2
 const chanceFor4Value = 0.25
 //0 based
-const dimensions = 3
+const dimensions = 4
 const tileEquality = (element: TileData) => { return element.id === this }
 const forward = 1, backwards = -1
 
@@ -81,6 +81,45 @@ export class GameComponent {
     this.spawnRandomTiles(2)
   }
 
+  moveTileHorizontal() {
+    let endpoint: TileData[] = this.tiles.map((e) => { return new TileData(e.id, e.value) })
+    let animations = new Map()
+    //loop through columns
+    for (let r = 0; r < dimensions; r++) {
+      //loop through row. Since this is a single dimension array mimicing a 2d array the formula is 4 * row + column
+      for (let c = dimensions * r + 1; c < dimensions * r + dimensions; c++) {
+        //finding the tiles from left -> right
+        if (endpoint[c].value > 0) {
+          //finding the tiles from right -> left
+          for (let i = c - 1; i >= dimensions * r; i--) { 
+            if (i == dimensions * r) { //checking if we hit the bounding box
+              let temp = endpoint[i]
+              endpoint[i] = endpoint[c]
+              endpoint[c] = temp
+            }
+            else if (endpoint[i].value > 0) { //checking for occupied tile
+              //combine the tiles
+              if (endpoint[i].value === endpoint[c].value) {
+                endpoint[i].value += endpoint[c].value
+                endpoint[c] = new TileData(this.tileId, 0)
+                //------------ TODO: add to animation
+              }
+              else {
+                //can't combine. Then move to the next slot to the right
+                //Empty slots is mock TileData so we can swap them
+                let temp = endpoint[i + 1]
+                endpoint[i + 1] = endpoint[c]
+                endpoint[c] = temp
+                //---------- TODO: add to animation map
+              }
+            }
+          }
+        }
+      }
+    }
+    this.tiles = endpoint
+  }
+
   keyDownHandler(event: KeyboardEvent) {
     let key = event.key
     switch (event.key) {
@@ -91,6 +130,7 @@ export class GameComponent {
         event.preventDefault()
         break
       case ('ArrowLeft'):
+        this.moveTileHorizontal()
         event.preventDefault()
         break
       case ('ArrowRight'):
